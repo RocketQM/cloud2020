e94e099b83e2b90c67444b8687e9de4e90355b39
JinKang <kangkang.jin@itrigger.com>
2020-07-01 12:18:21 +0800

    新增 zookeeper  Payment8004类

---
+++ cloud-provider-payment-8004/src/main/java/com/henu/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.henu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author jinkang
+ * @date 2020/7/1
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping("/payment/zk")
+    public String paymentzk(){
+        return "springcloud with zookeeper: " + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
